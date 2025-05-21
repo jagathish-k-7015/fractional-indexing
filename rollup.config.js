08955951229dcfaead5affee980ca38ac9ac8a93
jagathish.k <jagathish.k@zohocorp.com>
2025-05-21 16:15:56 +0530

    Used rollup to create ready to use FractionalIndexing feature for browser

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/fractional-indexing.js',
+      format: 'umd',
+      name: 'FractionalIndexing',
+      exports: 'named'
+    }
+  };
